cf816648540b33f421dd2388ccbce83447d04ced
Jsoriano <jsorianoh89@gmail.com>
2025-08-29 18:43:01 +0000

    Test jest

--- test.js
+++ test.js
@@ -4,12 +4,12 @@ test('convierte de dólares a yenes', () => {
     expect(fromDollarToYen(1)).toBeCloseTo(146.26);
 });
 
-const { fromEuroToDollar } = require("./app.js")
+const { fromEuroToDollar } = require("./app")
 test('convierte de euros a dólares', () => {
     expect(fromEuroToDollar(1)).toBeCloseTo(1.07);
 });
 
-const { fromYenToPound } = require("./app.js")
+const { fromYenToPound } = require("./app")
 test('convierte de yenes a libras', () => {
     expect(fromYenToPound(156.5)).toBeCloseTo(0.87); 
 });
